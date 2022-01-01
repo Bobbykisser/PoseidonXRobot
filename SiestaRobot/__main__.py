c1691a13eaea9a91d71dcbbbd7a2a6019228105e
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-01-01 14:31:38 +0530

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -788,7 +788,7 @@ def main():
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
             dispatcher.bot.sendMessage(
-                f"{@Stuxnet_1_official}", 
+                f"@{SUPPORT_CHAT}", 
                 f"""**Siesta Robot Started!**
 
 **Python:** `{memek()}`
