7291ad6d35a5ea12cd456fdb18c47d89bc3b3e11
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2022-01-01 23:05:46 +0530

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -246,7 +246,7 @@ def start(update: Update, context: CallbackContext):
     else:
         update.effective_message.reply_photo(
             GROUP_START_IMG, caption= f"<b>Poseidon will never die!</b>\n<b>Started working since:</b> <code>{uptime}</code>",
-            parse_mode=ParseMode.HTML
+
        
             ),
             
